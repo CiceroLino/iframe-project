68ea0bcb142cd86461127c7afe00ebee10beb25c
Cicero Lino <cicerolinoeneto@gmail.com>
2024-08-25 23:13:54 -0300

    refator aplication to new postgres driver connection

--- backend/src/models.ts
+++ backend/src/models.ts
@@ -1,8 +1,15 @@
 import { Sequelize, Model, DataTypes, UUID, UUIDV4 } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.sqlite3",
+  dialect: "postgres",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || "5432"),
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 class Demo extends Model {
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,16 @@
 import app from "./app";
 import { sequelize } from "./models";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
 
 async function init() {
   try {
     await sequelize.sync();
-    app.listen(3001, () => {
-      console.log("Express App Listening on Port 3001");
+    app.listen(PORT, () => {
+      console.log(`Express App Listening on Port ${PORT}`);
     });
   } catch (error) {
     console.error(`An error occurred: ${JSON.stringify(error)}`);
